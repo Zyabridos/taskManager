9a8579a77122e028b7296901b45b2dd938264157
Nina Zyabrina <zyabrina95@gmail.com>
2024-12-30 12:29:02 +0100

    add i18n, bootstrap webpack and pug settings and updt homepage acc tp plugins

--- server/index.js
+++ server/index.js
@@ -1,18 +1,47 @@
-import Fastify from 'fastify';
+import fastify from 'fastify';
+import fastifyView from '@fastify/view'; 
+import pug from 'pug';
+import i18next from 'i18next';
+import middleware from 'i18next-http-middleware';
+import path from 'path';
+import en from './locales/en.js';
+import ru from './locales/ru.js';
 
-const app = Fastify({ logger: true });
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-// Главный маршрут
-app.get('/', async (request, response) => ({ message: 'Hello, world!' }));
+const app = fastify({ logger: true });
 
-const start = async () => {
-  try {
-    await app.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Server runs on http://localhost:3000');
-  } catch (err) {
+i18next
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    preload: ['en', 'ru'],
+    resources: {
+      en,
+      ru
+    }
+  });
+
+app.register(middleware.plugin, { i18next });
+
+app.register(fastifyView, {
+  engine: { pug },
+});
+
+// Роут для главной страницы с локализацией
+app.get('/', async (req, res) => {
+  const t = req.t; // функция перевода
+  return res.view('./server/views/index.pug', {
+    title: t('title'),
+    message: t('message'),
+  });
+});
+
+// Запуск сервера
+app.listen({ port: 3000 }, (err) => {
+  if (err) {
     app.log.error(err);
     process.exit(1);
   }
-};
-
-start();
+  app.log.info('Server is running on http://localhost:3000');
+});
---
+++ server/locales/en.js
@@ -0,0 +1,4 @@
+export default {
+  "title": "Home Page",
+  "message": "Welcome to our service!"
+}
