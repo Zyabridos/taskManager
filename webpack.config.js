b70bf3044a01d4b8001c4f0b49305ba591e99b16
Nina Zyabrina <zyabrina95@gmail.com>
2025-02-03 22:19:31 +0100

    reinstall deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,16 @@
-import path from 'path';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { fileURLToPath } from 'url';
 
 const mode = process.env.NODE_ENV || 'development';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default {
   mode,
-  entry: './server/startServer.js', // Основной файл бэкенда
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'server.bundle.js',
-  },
-  target: 'node',
   module: {
     rules: [
       {
         test: /\.css$/,
-        // use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
-  // plugins: [new MiniCssExtractPlugin()],
-  devServer: {
-    port: 5000, // Сервер бэкенда
-    hot: true,
-  },
+  plugins: [new MiniCssExtractPlugin()],
 };
