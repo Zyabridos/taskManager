fe090e768361832d48959623b97b4cc109c4928e
Nina Zyabrina <zyabrina95@gmail.com>
2025-03-10 17:24:19 +0100

    change port

--- server/startServer.js
+++ server/startServer.js
@@ -5,25 +5,19 @@ const app = fastify({ logger: true });
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
-  await init(app);
+  try {
+    await init(app);
 
-  app.ready((err) => {
-    if (err) throw err;
-  });
+    await app.ready();
+    app.log.info("All plugins initialized successfully.");
 
-  app.listen(
-    {
-      port: PORT,
-      host: "0.0.0.0",
-    },
-    (err, address) => {
-      if (err) {
-        app.log.error(err);
-        process.exit(1);
-      }
-      app.log.info(`Server is running on ${address}`);
-    },
-  );
+    await app.listen({ port: PORT, host: "0.0.0.0" });
+
+    app.log.info(`Server is running on http://localhost:${PORT}`);
+  } catch (err) {
+    app.log.error("Error starting the server:", err);
+    process.exit(1);
+  }
 };
 
 startServer();
