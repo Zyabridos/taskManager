c25105191000be80332015d7401e38d616362ffd
Nina Zyabrina <zyabrina95@gmail.com>
2025-03-27 12:18:05 +0100

    split server start into two files

--- server/index.js
+++ server/index.js
@@ -2,25 +2,11 @@ import fastify from 'fastify';
 
 import init from './plugin/init.js';
 
-const app = fastify({ logger: true });
-const PORT = process.env.PORT || 3000;
-
-const startServer = async () => {
-  try {
-    await init(app);
-
-    await app.ready();
-    app.log.info('All plugins initialized successfully.');
-
-    await app.listen({ port: PORT, host: '0.0.0.0' });
-
-    app.log.info(`Server is running on http://localhost:${PORT}`);
-  } catch (err) {
-    app.log.error('Error starting the server:', err);
-    process.exit(1);
-  }
+const buildApp = async () => {
+  const app = fastify({ logger: true });
+  await init(app);
+  await app.ready();
+  return app;
 };
 
-startServer();
-
-export default startServer;
+export default buildApp;
