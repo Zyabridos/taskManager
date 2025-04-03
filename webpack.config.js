97004fc5fd5772f3fd4f03be8abc234aeb85f77a
Nina Zyabrina <zyabrina95@gmail.com>
2025-04-03 21:11:09 +0200

    updt assets paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
-/* eslint-disable import/no-extraneous-dependencies */
+import path from 'path';
+
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const mode = process.env.NODE_ENV || 'development';
 
 export default {
   mode,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false, // оставляем отключённым, чтобы пути не переписывались
+            },
+          },
+        ],
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'assets/main.css' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/assets', to: 'assets/images' },
+      ],
+    }),
+  ],
 };
