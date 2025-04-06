98ca16cdfbcd418e7616127bc4a29c551c12e80c
Nina Zyabrina <zyabrina95@gmail.com>
2025-04-06 16:31:47 +0200

    add deps for back

--- backend/server/plugin/init.js
+++ backend/server/plugin/init.js
@@ -1,13 +1,11 @@
 import addRoutes from '../routes/index.js';
 
 import registerPlugins from './plugins/registerPlugins.js';
-import setUpViews from './config/setupViews.js';
 import setUpStaticAssets from './config/setupStaticAssets.js';
 import addHooks from './middlewares/addHooks.js';
 
 const init = async (app) => {
   await registerPlugins(app);
-  setUpViews(app);
   setUpStaticAssets(app);
   await addRoutes(app);
   addHooks(app);
