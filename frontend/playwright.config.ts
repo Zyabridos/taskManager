5ee14936ba190eabaaac2c9e21b726938b8920d3
Nina Zyabrina <zyabrina95@gmail.com>
2025-04-11 21:16:09 +0200

    updt playwright settings

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 5000,
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 10000,
+  },
   use: {
     baseURL: 'http://localhost:3000',
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
   webServer: {
-    command: 'make dev-frontend',
+    command: 'PORT=3000 npm run start',
     port: 3000,
-    cwd: '..',
-    reuseExistingServer: true,
+    cwd: './frontend',
+    reuseExistingServer: false,
+    timeout: 60 * 1000,
   },
 });
